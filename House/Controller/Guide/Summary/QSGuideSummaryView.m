4d1a6a3540870421732bd56834acf6c282f1ccd8
ysmeng <49427823@163.com>
2015-01-20 16:11:21 +0800

    添加汇总指引页头信息-已完成-杨声孟

--- House/Controller/Guide/Summary/QSGuideSummaryView.m
+++ House/Controller/Guide/Summary/QSGuideSummaryView.m
@@ -11,59 +11,169 @@
 
 @implementation QSGuideSummaryView
 
-#pragma mark - 初始化
-- (instancetype)initWithFrame:(CGRect)frame
+#pragma mark - UI搭建
+- (void)createCustomGuideHeaderSubviewsUI:(UIView *)view
 {
 
-    if (self = [super initWithFrame:frame]) {
-        
-        ///搭建UI
-        [self createGuideSummaryUI];
-        
-    }
+    ///头信息按上下两半切分：上面是房源，下面是房客
+    UIView *housesView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, view.frame.size.width, view.frame.size.height/2.0f)];
+    housesView.backgroundColor = [UIColor clearColor];
+    [self createHousesSubviews:housesView];
+    [view addSubview:housesView];
     
-    return self;
+    UIView *tenantView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, view.frame.size.height/2.0f, view.frame.size.width, view.frame.size.height/2.0f)];
+    tenantView.backgroundColor = [UIColor clearColor];
+    [self createTenantSubviews:tenantView];
+    [view addSubview:tenantView];
 
 }
 
-#pragma mark - UI搭建
-/**
- *  @author yangshengmeng, 15-01-20 11:01:13
- *
- *  @brief  搭建汇总指引页的UI
- *
- *  @since  1.0.0
- */
-- (void)createGuideSummaryUI
+///添加房源信息展示view
+- (void)createHousesSubviews:(UIView *)view
 {
 
+    ///房源:173 x 200
+    UIImageView *houseHavedTipsImageView = [[UIImageView alloc] init];
+    houseHavedTipsImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    houseHavedTipsImageView.image = [UIImage imageNamed:IMAGE_GUIDE_HOUSESTIP];
+    [view addSubview:houseHavedTipsImageView];
+    
+    ///房源数量人头图片
+    UIImageView *houseHavedImageView = [[UIImageView alloc] init];
+    houseHavedImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    houseHavedImageView.image = [UIImage imageNamed:IMAGE_GUIDE_HOUSES_PERSION];
+    [view addSubview:houseHavedImageView];
+    
+    ///数量显示Label
+    QSLabel *countLabel = [[QSLabel alloc] init];
+    countLabel.font = [UIFont boldSystemFontOfSize:FONT_BODY_35];
+    countLabel.text = @"234,8923";
+    countLabel.textColor = [UIColor blackColor];
+    countLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [view addSubview:countLabel];
+    
+    ///约束view字典
+    NSDictionary *___VFLViewsDict = NSDictionaryOfVariableBindings(houseHavedImageView,houseHavedTipsImageView,countLabel);
+    
+    ///添加约束
+    NSString *___hVFL_tipAndHaved = @"H:|-(>=15)-[houseHavedImageView(165)]-[houseHavedTipsImageView(87)]-(>=15)-|";
+    NSString *___hVFL_countLabel = @"H:[countLabel(>=165)]";
+    NSString *___vVFL_havedTip = @"V:[houseHavedTipsImageView(100)]";
+    NSString *___vVFL_havedImageAndCountLabel = @"V:[houseHavedImageView(28)]-[countLabel(houseHavedImageView)]-15-|";
+    
+    ///添加结束
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___hVFL_tipAndHaved options:NSLayoutFormatAlignAllLastBaseline metrics:nil views:___VFLViewsDict]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___vVFL_havedTip options:NSLayoutFormatAlignAllCenterY metrics:nil views:___VFLViewsDict]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___vVFL_havedImageAndCountLabel options:NSLayoutFormatAlignAllLeft metrics:nil views:___VFLViewsDict]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___hVFL_countLabel options:0 metrics:nil views:___VFLViewsDict]];
+
+}
+
+///添加房客信息展示view
+- (void)createTenantSubviews:(UIView *)view
+{
+    
+    ///房客:173 x 200
+    UIImageView *tenantHavedTipsImageView = [[UIImageView alloc] init];
+    tenantHavedTipsImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    tenantHavedTipsImageView.image = [UIImage imageNamed:IMAGE_GUIDE_HOUSESTIP];
+    [view addSubview:tenantHavedTipsImageView];
+    
+    ///房客数量人头图片
+    UIImageView *tenantHavedImageView = [[UIImageView alloc] init];
+    tenantHavedImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    tenantHavedImageView.image = [UIImage imageNamed:IMAGE_GUIDE_HOUSES_PERSION];
+    [view addSubview:tenantHavedImageView];
+    
+    ///数量显示Label
+    QSLabel *countLabel = [[QSLabel alloc] init];
+    countLabel.font = [UIFont boldSystemFontOfSize:FONT_BODY_35];
+    countLabel.text = @"234,8923";
+    countLabel.textColor = [UIColor blackColor];
+    countLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [view addSubview:countLabel];
+    
+    ///约束view字典
+    NSDictionary *___VFLViewsDict = NSDictionaryOfVariableBindings(tenantHavedTipsImageView,tenantHavedImageView,countLabel);
+    
+    ///添加约束
+    NSString *___hVFL_tipAndHaved = @"H:|-15-[tenantHavedTipsImageView(87)]-[tenantHavedImageView(165)]-(>=15)-|";
+    NSString *___hVFL_countLabel = @"H:[countLabel(>=165)]";
+    NSString *___vVFL_havedTip = @"V:[tenantHavedTipsImageView(100)]";
+    NSString *___vVFL_havedImageAndCountLabel = @"V:[tenantHavedImageView(28)]-[countLabel(tenantHavedImageView)]-15-|";
+    
+    ///添加结束
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___hVFL_tipAndHaved options:NSLayoutFormatAlignAllLastBaseline metrics:nil views:___VFLViewsDict]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___vVFL_havedTip options:NSLayoutFormatAlignAllCenterY metrics:nil views:___VFLViewsDict]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___vVFL_havedImageAndCountLabel options:NSLayoutFormatAlignAllRight metrics:nil views:___VFLViewsDict]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___hVFL_countLabel options:0 metrics:nil views:___VFLViewsDict]];
+    
+}
+
+/**
+ *  @author     yangshengmeng, 15-01-20 14:01:51
+ *
+ *  @brief      在给定的视图上添加底部相关控件
+ *
+ *  @param view 底部控制
+ *
+ *  @since      1.0.0
+ */
+- (void)createCustomGuideFooterUI:(UIView *)view
+{
+    
     ///我要找房
     QSBlockButtonStyleModel *yellowButtonStyle = [QSBlockButtonStyleModel createNormalButtonWithType:nNormalButtonTypeCornerYellow];
     yellowButtonStyle.title = TITLE_GUIDE_SUMMARY_FINDHOUSE_BUTTON;
-    UIButton *findHouseButton = [UIButton createBlockButtonWithFrame:CGRectMake((SIZE_DEVICE_WIDTH - 160.0f)/2.0f, SIZE_DEVICE_HEIGHT - 20.0f - 44.0f * 2.0f - 8.0f, 160.0f, VIEW_SIZE_NORMAL_BUTTON_HEIGHT) andButtonStyle:yellowButtonStyle andCallBack:^(UIButton *button) {
+    UIButton *findHouseButton = [UIButton createBlockButtonWithButtonStyle:yellowButtonStyle andCallBack:^(UIButton *button) {
         
         [self findHouse];
         
     }];
-    [self addSubview:findHouseButton];
+    findHouseButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [view addSubview:findHouseButton];
     
     ///我要找房
     QSBlockButtonStyleModel *whiteButtonStyle = [QSBlockButtonStyleModel createNormalButtonWithType:nNormalButtonTypeCornerWhite];
     whiteButtonStyle.title = TITLE_GUIDE_SUMMARY_SALEHOUSE_BUTTON;
-    UIButton *saleHouseButton = [UIButton createBlockButtonWithFrame:CGRectMake((SIZE_DEVICE_WIDTH - 160.0f)/2.0f, SIZE_DEVICE_HEIGHT - 20.0f - 44.0f, 160.0f, VIEW_SIZE_NORMAL_BUTTON_HEIGHT) andButtonStyle:whiteButtonStyle andCallBack:^(UIButton *button) {
+    UIButton *saleHouseButton = [UIButton createBlockButtonWithButtonStyle:whiteButtonStyle andCallBack:^(UIButton *button) {
         
         [self saleHouse];
         
     }];
-    [self addSubview:saleHouseButton];
-
+    saleHouseButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [view addSubview:saleHouseButton];
+    
+    ///计算间隙
+    CGFloat gap = (SIZE_DEVICE_HEIGHT > 480.0f) ? ((SIZE_DEVICE_WIDTH > 320.0f) ? ((SIZE_DEVICE_WIDTH > 350.0f) ? 40.0f : 30.0f) : 30.0f) : 20.0f;
+    CGFloat width = (SIZE_DEVICE_WIDTH > 320.0f) ? ((SIZE_DEVICE_WIDTH > 350.0f) ? 300.0f : 240.0f) : 200.0f;
+    NSDictionary *___VFLSizeDict = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithFormat:@"%.2f",gap],@"gap",[NSString stringWithFormat:@"%.2f",width],@"width",nil];
+    
+    ///添加约束
+    NSString *___hVFL_findButton = @"H:[findHouseButton(width)]";
+    NSString *___hVFL_saleButton = @"H:[saleHouseButton(width)]";
+    NSString *___vVFL_all = @"V:|-gap-[findHouseButton]-10-[saleHouseButton(==findHouseButton)]-gap-|";
+    
+    ///约束参数字典
+    NSDictionary *___VFLViewsDict = NSDictionaryOfVariableBindings(findHouseButton,saleHouseButton);
+    
+    ///添加约束
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___hVFL_findButton options:NSLayoutFormatAlignAllCenterY metrics:___VFLSizeDict views:___VFLViewsDict]];
+    [view addConstraint:[NSLayoutConstraint constraintWithItem:findHouseButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___hVFL_saleButton options:NSLayoutFormatAlignAllCenterY metrics:___VFLSizeDict views:___VFLViewsDict]];
+    [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:___vVFL_all options:NSLayoutFormatAlignAllCenterX metrics:___VFLSizeDict views:___VFLViewsDict]];
+    
 }
 
 #pragma mark - 点击<我要找房>
 - (void)findHouse
 {
 
-    
+    if (self.guideButtonCallBack) {
+        
+        self.guideButtonCallBack(gGuideButtonActionTypeFindHouse);
+        
+    }
 
 }
 
@@ -71,7 +181,11 @@
 - (void)saleHouse
 {
 
-    
+    if (self.guideButtonCallBack) {
+        
+        self.guideButtonCallBack(gGuideButtonActionTypeSaleHouse);
+        
+    }
 
 }
 
--- House/Core/Button/QSBlockButton.h
+++ House/Core/Button/QSBlockButton.h
@@ -12,6 +12,20 @@
 @interface UIButton (QSBlockButton)
 
 /**
+ *  @author             yangshengmeng, 15-01-20 14:01:46
+ *
+ *  @brief              创建一个没有相对位置和大小的指定风格按钮，方便自适应使用
+ *
+ *  @param buttonStyle  按钮风格模型
+ *  @param callBack     单击按钮时的回调
+ *
+ *  @return             返回创建的按钮
+ *
+ *  @since              1.0.0
+ */
++ (UIButton *)createBlockButtonWithButtonStyle:(QSBlockButtonStyleModel *)buttonStyle andCallBack:(void(^)(UIButton *button))callBack;
+
+/**
  *  @author             yangshengmeng, 15-01-17 17:01:04
  *
  *  @brief              创建并返回一个特定风格并且带有回调的按钮
@@ -38,6 +52,19 @@
 @interface QSBlockButton : UIButton
 
 @property (nonatomic,copy) void(^blockButtonCallBack)(UIButton *button);//!<单击时的回调
+
+/**
+ *  @author             yangshengmeng, 15-01-20 14:01:44
+ *
+ *  @brief              创建一个没有frame的按钮
+ *
+ *  @param buttonStyle  按钮的风格
+ *
+ *  @return             返回当前按钮
+ *
+ *  @since              1.0.0
+ */
+- (instancetype)initWithButtonStyle:(QSBlockButtonStyleModel *)buttonStyle;
 
 /**
  *  @author             yangshengmeng, 15-01-17 18:01:29
