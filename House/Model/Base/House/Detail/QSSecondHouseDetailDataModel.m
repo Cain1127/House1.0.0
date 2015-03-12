6a3bcf801da9101adf7f0a76df2efe5876ac010a
wsp2046 <1945959282@qq.com>
2015-03-12 10:22:14 +0800

    接入二手房网络数据请求--王树朋

---
+++ House/Model/Base/House/Detail/QSSecondHouseDetailDataModel.m
@@ -0,0 +1,13 @@
+//
+//  QSSecondHouseDetailDataModel.m
+//  House
+//
+//  Created by 王树朋 on 15/3/12.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import "QSSecondHouseDetailDataModel.h"
+
+@implementation QSSecondHouseDetailDataModel
+
+@end
