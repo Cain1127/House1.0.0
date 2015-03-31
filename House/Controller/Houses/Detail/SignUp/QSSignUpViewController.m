fbb9483b8a9c9380578365731d42c3ce1d55b6da
wsp2046 <1945959282@qq.com>
2015-03-31 15:33:59 +0800

    报名界面逻辑修改--王树朋

--- House/Controller/Houses/Detail/SignUp/QSSignUpViewController.m
+++ House/Controller/Houses/Detail/SignUp/QSSignUpViewController.m
@@ -17,9 +17,13 @@
 
 ///关联
 static char BottomViewKey;  //!<底部按钮关联key
-static char RootViewKey;     //!<顶部信息关联key
+static char RootViewKey;    //!<顶部信息关联key
+static char MiddleViewKey;  //!<中间输入框关联key
+static char linkManViewKey; //!<联系人关联key
+static char phoneViewKey;   //!<联系电话关联key
+static char numberViewKey;  //!<报名人数关联key
 
-@interface QSSignUpViewController ()
+@interface QSSignUpViewController ()<UITextFieldDelegate>
 
 @property (nonatomic,copy) NSString *activityID;  //!<活动ID
 @property (nonatomic,copy) NSString *title;       //!<活动标题
@@ -102,6 +106,7 @@ static char RootViewKey;     //!<顶部信息关联key
     UIView *middleView=[[UIView alloc] initWithFrame:CGRectMake(25.0f, topView.frame.origin.y+topView.frame.size.height, SIZE_DEVICE_WIDTH-50.0f, 44.0f * 3.0f)];
     [rootView addSubview:middleView];
     [self createMiddleUI:middleView];
+    objc_setAssociatedObject(self, &MiddleViewKey, middleView, OBJC_ASSOCIATION_ASSIGN);
     
 }
 
@@ -109,7 +114,6 @@ static char RootViewKey;     //!<顶部信息关联key
 {
     
     QSLabel *titleLabel=[[QSLabel alloc] initWithFrame:CGRectMake(0.0f, 20.0f, SIZE_DEVICE_WIDTH, 20.0f)];
-    titleLabel.textAlignment=NSTextAlignmentCenter;
     
     if (self.activityID) {
         
@@ -119,6 +123,8 @@ static char RootViewKey;     //!<顶部信息关联key
     titleLabel.text=self.houseTitle;
 
     titleLabel.font=[UIFont systemFontOfSize:20.0f];
+    titleLabel.textAlignment=NSTextAlignmentCenter;
+
     [view addSubview:titleLabel];
     
     if (self.activityID) {
@@ -145,7 +151,11 @@ static char RootViewKey;     //!<顶部信息关联key
 
     UITextField *linkMan = [UITextField createCustomTextFieldWithFrame:CGRectMake(0.0f, 0.0f, view.frame.size.width, 44.0f) andPlaceHolder:nil andLeftTipsInfo:@"联系人:" andLeftTipsTextAlignment:NSTextAlignmentCenter andTextFieldStyle:cCustomTextFieldStyleLeftTipsGray];
     linkMan.clearButtonMode = UITextFieldViewModeWhileEditing;
+    linkMan.delegate = self;
+    //linkMan.keyboardType = UIKeyboardTypeASCIICapable;
     [view addSubview:linkMan];
+    objc_setAssociatedObject(self, &linkManViewKey, linkMan, OBJC_ASSOCIATION_ASSIGN);
+    
     
     ///分隔线
     UILabel *linkManLineLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f,linkMan.frame.size.height- 0.25f, linkMan.frame.size.width,  0.25f)];
@@ -153,9 +163,13 @@ static char RootViewKey;     //!<顶部信息关联key
     [view addSubview:linkManLineLabel];
     
     
-    UITextField *phone=[UITextField createCustomTextFieldWithFrame:CGRectMake(0.0f, linkMan.frame.origin.y+linkMan.frame.size.height, view.frame.size.width, 44.0f) andPlaceHolder:nil andLeftTipsInfo:@"联系人电话:" andLeftTipsTextAlignment:NSTextAlignmentCenter andTextFieldStyle:cCustomTextFieldStyleLeftTipsGray];
-    phone.clearButtonMode=UITextFieldViewModeWhileEditing;
+    UITextField *phone = [UITextField createCustomTextFieldWithFrame:CGRectMake(0.0f, linkMan.frame.origin.y+linkMan.frame.size.height, view.frame.size.width, 44.0f) andPlaceHolder:nil andLeftTipsInfo:@"联系人电话:" andLeftTipsTextAlignment:NSTextAlignmentCenter andTextFieldStyle:cCustomTextFieldStyleLeftTipsGray];
+    
+    phone.clearButtonMode = UITextFieldViewModeWhileEditing;
+    phone.delegate = self;
+    phone.keyboardType = UIKeyboardTypeNumberPad;
     [view addSubview:phone];
+    objc_setAssociatedObject(self, &phoneViewKey, phone, OBJC_ASSOCIATION_ASSIGN);
     
     ///分隔线
     UILabel *phoneLineLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f,phone.frame.origin.y+phone.frame.size.height- 0.25f, phone.frame.size.width,  0.25f)];
@@ -164,9 +178,13 @@ static char RootViewKey;     //!<顶部信息关联key
     
     if (self.activityID) {
         
-        UITextField *SignUpNum=[UITextField createCustomTextFieldWithFrame:CGRectMake(0.0f, phone.frame.origin.y+phone.frame.size.height, view.frame.size.width, 44.0f) andPlaceHolder:nil andLeftTipsInfo:@"报名人数:" andLeftTipsTextAlignment:NSTextAlignmentCenter andTextFieldStyle:cCustomTextFieldStyleLeftTipsGray];
-        SignUpNum.clearButtonMode=UITextFieldViewModeWhileEditing;
+        UITextField *SignUpNum = [UITextField createCustomTextFieldWithFrame:CGRectMake(0.0f, phone.frame.origin.y+phone.frame.size.height, view.frame.size.width, 44.0f) andPlaceHolder:nil andLeftTipsInfo:@"报名人数:" andLeftTipsTextAlignment:NSTextAlignmentCenter andTextFieldStyle:cCustomTextFieldStyleLeftTipsGray];
+        
+        SignUpNum.clearButtonMode = UITextFieldViewModeWhileEditing;
+        SignUpNum.delegate=self;
+        SignUpNum.keyboardType=UIKeyboardTypeNumberPad;
         [view addSubview:SignUpNum];
+        objc_setAssociatedObject(self, &numberViewKey, SignUpNum, OBJC_ASSOCIATION_ASSIGN);
         
         ///分隔线
         UILabel *SignUpNumLineLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f,SignUpNum.frame.origin.y+SignUpNum.frame.size.height- 0.25f, SignUpNum.frame.size.width,  0.25f)];
@@ -192,16 +210,104 @@ static char RootViewKey;     //!<顶部信息关联key
     buttonStyle.cornerRadio=6.0f;
     buttonStyle.titleNormalColor=COLOR_CHARACTERS_BLACK;
     
+    UITextField *linkManView = objc_getAssociatedObject(self, &linkManViewKey);
+    UITextField *phoneView = objc_getAssociatedObject(self, &phoneViewKey);
+    UITextField *numberView = objc_getAssociatedObject(self, &numberViewKey);
     
     UIButton *signUpButton=[QSBlockButton createBlockButtonWithFrame:CGRectMake(SIZE_DEFAULT_MARGIN_LEFT_RIGHT, 8.0f, SIZE_DEFAULT_MAX_WIDTH, 44.0f) andButtonStyle:buttonStyle andCallBack:^(UIButton *button) {
         
         APPLICATION_LOG_INFO(@"提交按钮事件", nil);
+        
+        ///联系人有效性数据
+        NSString *linkManString = linkManView.text;
+        if ([linkManString length] <=0) {
+            
+            TIPS_ALERT_MESSAGE_ANDTURNBACK(@"请输入联系人姓名", 1.0f, ^(){
+                
+                [linkManView becomeFirstResponder];
+                
+            })
+            return;
+        }
+        
+        
+        ///手机有效性数据
+        NSString *phoneString = phoneView.text;
+        if ([phoneString length] <= 0) {
+            
+            
+            TIPS_ALERT_MESSAGE_ANDTURNBACK(@"请输入手机号码", 1.0f, ^(){
+                
+                [phoneView becomeFirstResponder];
+                
+            })
+            return;
+            
+        }
+        
+        if (![NSString isValidateMobile:phoneString]) {
+            
+            TIPS_ALERT_MESSAGE_ANDTURNBACK(@"手机号码应为11位数字，以13/14/15/17/18开头", 1.0f, ^(){
+                
+                [phoneView becomeFirstResponder];
+                
+            })
+            return;
+        }
+
+        ///报名人数有效性数据
+        NSString *numberString = numberView.text;
+        if ([numberString length] <= 0) {
+            
+            TIPS_ALERT_MESSAGE_ANDTURNBACK(@"请输入报名人数", 1.0f, ^(){
+                
+                [linkManView becomeFirstResponder];
+                
+            })
+            
+            return;
+        }
+        
+        ///回收键盘
+        [phoneView resignFirstResponder];
+        [numberView resignFirstResponder];
+        [linkManView resignFirstResponder];
+        
+        ///网络请求
+        
         
     }];
     [view addSubview:signUpButton];
     
 }
 
+#pragma mark - 回调键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    
+    [textField resignFirstResponder];
+    
+    return YES;
+    
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+    UIView *middleView = objc_getAssociatedObject(self, &MiddleViewKey);
+    for (UIView *obj in [middleView subviews]) {
+        
+        if ([obj isKindOfClass:[UITextField class]]) {
+            
+            [((UITextField *)obj) resignFirstResponder];
+            
+        }
+        
+    }
+    
+}
+
+#pragma mark -创建UI结束刷新
 -(void)getSignUpInfo
 {
 
