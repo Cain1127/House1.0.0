19352cf97879a437cb085bba7d656f238edad25e
wsp2046 <1945959282@qq.com>
2015-04-11 12:48:17 +0800

    修正ink错误-王树朋

---
+++ House/Controller/Houses/Detail/CommunityHouseList/List/QSWCommunityRentHouseList.h
@@ -0,0 +1,18 @@
+//
+//  QSWCommunityRentHouseList.h
+//  House
+//
+//  Created by 王树朋 on 15/4/11.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class QSFilterDataModel;
+@interface QSWCommunityRentHouseList : UICollectionView
+
+- (instancetype)initWithFrame:(CGRect)frame andCommunitID:(NSString *)communityID andFilter:(QSFilterDataModel *)filterModel andCallBack:(void(^)(HOUSE_LIST_ACTION_TYPE actionType,id tempModel))callBack;
+
+- (void)reloadServerData:(QSFilterDataModel *)filterModel;
+
+@end
