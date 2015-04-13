a0c7d3854fb5a5e99e26d1f3aaaf17e483f44ee7
ysmeng <49427823@163.com>
2015-04-13 15:38:55 +0800

    修改发现模块中的工具相关功能的UI-杨声孟

--- House/Controller/Chat/Setting/QAA/QSYToolQuestionAndAnswerViewController.m
+++ House/Controller/Chat/Setting/QAA/QSYToolQuestionAndAnswerViewController.m
@@ -9,6 +9,8 @@
 #import "QSYToolQuestionAndAnswerViewController.h"
 #import "QSYToolQAADetailViewController.h"
 
+#import "QSBlockButtonStyleModel+Normal.h"
+
 @interface QSYToolQuestionAndAnswerViewController ()
 
 @end
@@ -22,6 +24,88 @@
     [super createNavigationBarUI];
     [self setNavigationBarTitle:@"法律指南"];
 
+}
+
+- (void)createMainShowUI
+{
+    
+    ///指示三角指针
+    __block UIImageView *arrowIndicator;
+    
+    ///按钮指针
+    __block UIButton *rentGuideButton;
+    __block UIButton *salGuideButton;
+    
+    ///尺寸
+    CGFloat widthButton = SIZE_DEVICE_WIDTH / 2.0f;
+    
+    ///按钮风格
+    QSBlockButtonStyleModel *buttonStyle = [QSBlockButtonStyleModel createNormalButtonWithType:nNormalButtonTypeClearGray];
+    buttonStyle.bgColorSelected = COLOR_CHARACTERS_LIGHTYELLOW;
+    buttonStyle.titleFont = [UIFont systemFontOfSize:FONT_BODY_16];
+    
+    ///租房指南
+    buttonStyle.title = @"租房指南";
+    rentGuideButton = [UIButton createBlockButtonWithFrame:CGRectMake(0.0f, 64.0f, widthButton, 44.0f) andButtonStyle:buttonStyle andCallBack:^(UIButton *button) {
+        
+        ///当前已处于选择状态
+        if (button.selected) {
+            
+            return;
+            
+        }
+        
+        ///切换按钮状态
+        button.selected = YES;
+        salGuideButton.selected = NO;
+        
+        ///移动指示器
+        [UIView animateWithDuration:0.3f animations:^{
+            
+            arrowIndicator.frame = CGRectMake(button.frame.origin.x + button.frame.size.width / 2.0f - 7.5f, arrowIndicator.frame.origin.y, arrowIndicator.frame.size.width, arrowIndicator.frame.size.height);
+            
+        } completion:^(BOOL finished) {
+            
+        }];
+        
+    }];
+    rentGuideButton.selected = YES;
+    [self.view addSubview:rentGuideButton];
+    
+    ///售房指南
+    buttonStyle.title = @"售房指南";
+    salGuideButton = [UIButton createBlockButtonWithFrame:CGRectMake(rentGuideButton.frame.origin.x + rentGuideButton.frame.size.width, 64.0f, widthButton, 44.0f) andButtonStyle:buttonStyle andCallBack:^(UIButton *button) {
+        
+        ///当前已处于选择状态
+        if (button.selected) {
+            
+            return;
+            
+        }
+        
+        ///切换按钮状态
+        button.selected = YES;
+        rentGuideButton.selected = NO;
+        
+        ///移动指示器
+        [UIView animateWithDuration:0.3f animations:^{
+            
+            arrowIndicator.frame = CGRectMake(button.frame.origin.x + button.frame.size.width / 2.0f - 7.5f, arrowIndicator.frame.origin.y, arrowIndicator.frame.size.width, arrowIndicator.frame.size.height);
+            
+        } completion:^(BOOL finished) {
+            
+        }];
+        
+    }];
+    [self.view addSubview:salGuideButton];
+    
+    ///初始化时，加载租房指南列表
+    
+    ///指示三角
+    arrowIndicator = [[QSImageView alloc] initWithFrame:CGRectMake(rentGuideButton.frame.origin.x+rentGuideButton.frame.size.width / 2.0f - 7.5f, rentGuideButton.frame.origin.y + rentGuideButton.frame.size.height - 5.0f, 15.0f, 5.0f)];
+    arrowIndicator.image = [UIImage imageNamed:IMAGE_CHANNELBAR_INDICATE_ARROW];
+    [self.view addSubview:arrowIndicator];
+    
 }
 
 @end
