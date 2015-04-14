d837056e1dab1d28b0c71034d5bee70e7245b52a
wsp2046 <1945959282@qq.com>
2015-04-14 15:12:52 +0800

    配置文件

---
+++ House/Controller/Developer/QSWDeveloperActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  QSWDeveloperActivityViewController.h
+//  House
+//
+//  Created by 王树朋 on 15/4/14.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import "QSTurnBackViewController.h"
+
+@interface QSWDeveloperActivityViewController : QSTurnBackViewController
+
+@end
---
+++ House/Controller/Developer/QSWDeveloperActivityViewController.m
@@ -0,0 +1,38 @@
+//
+//  QSWDeveloperActivityViewController.m
+//  House
+//
+//  Created by 王树朋 on 15/4/14.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import "QSWDeveloperActivityViewController.h"
+
+@interface QSWDeveloperActivityViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@end
+
+@implementation QSWDeveloperActivityViewController
+
+-(void)createNavigationBarUI
+{
+    
+    [super createNavigationBarUI];
+    [self setNavigationBarTitle:@"活动"];
+
+}
+
+-(void)createMainShowUI
+{
+
+    
+
+}
+
+#pragma mark -代理方法
+//-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+//{
+//
+//
+//}
+@end
