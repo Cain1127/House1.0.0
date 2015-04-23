7f52ed4236f8576819dc3281599b69543946006a
ysmeng <49427823@163.com>
2015-04-23 12:01:15 +0800

    二手房中，添加新的解析字段-杨声孟

--- House/Model/Base/House/House/QSWSecondHouseInfoDataModel.h
+++ House/Model/Base/House/House/QSWSecondHouseInfoDataModel.h
@@ -10,11 +10,15 @@
 
 @interface QSWSecondHouseInfoDataModel : QSHouseInfoDataModel
 
-@property (nonatomic,copy) NSString *price_avg;                  //!<小区均价
-@property (nonatomic,copy) NSString *coordinate_x;               //!<经度
-@property (nonatomic,copy) NSString *coordinate_y;               //!<纬度
+@property (nonatomic,copy) NSString *price_avg;                 //!<小区均价
+@property (nonatomic,copy) NSString *coordinate_x;              //!<经度
+@property (nonatomic,copy) NSString *coordinate_y;              //!<纬度
+@property (nonatomic,copy) NSString *tj_condition;              //!<房源内部评分
+@property (nonatomic,copy) NSString *tj_environment;            //!<周边环境评分
+@property (nonatomic,copy) NSString *book_num;                  //!<当前房源预约订单数量
+@property (nonatomic,copy) NSString *is_store;                  //!<当前用户是否已收藏
 
-@property (nonatomic,copy) NSString *tj_look_house_num;          //!<已看房人数
-@property (nonatomic,copy) NSString *tj_wait_look_house_people;  //!<待看房数
+@property (nonatomic,copy) NSString *tj_look_house_num;         //!<已看房人数
+@property (nonatomic,copy) NSString *tj_wait_look_house_people; //!<待看房数
 
 @end
