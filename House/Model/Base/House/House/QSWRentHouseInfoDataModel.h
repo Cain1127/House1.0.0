58f848f151354da0fad1b27d766ebe9dd17e6124
ysmeng <49427823@163.com>
2015-04-23 16:27:20 +0800

    恢复出租房详情中的数据模型-杨声孟

--- House/Model/Base/House/House/QSWRentHouseInfoDataModel.h
+++ House/Model/Base/House/House/QSWRentHouseInfoDataModel.h
@@ -17,9 +17,16 @@
 @property (nonatomic,copy) NSString *tj_look_house_num;         //!<已看房人数
 @property (nonatomic,copy) NSString *tj_wait_look_house_people; //!<待看房数
 @property (nonatomic,copy) NSString *price_avg;                 //!<小区均价
+@property (nonatomic,copy) NSString *create_time;               //!<数据创建时间
+@property (nonatomic,copy) NSString *is_store;                  //!<当前是否已收藏
 
-@property (nonatomic,copy) NSString *coordinate_x;               //!<经度
-@property (nonatomic,copy) NSString *coordinate_y;               //!<纬度
+@property (nonatomic,copy) NSString *tj_condition;              //!<内部条件评分
+@property (nonatomic,copy) NSString *tj_environment;            //!<周边环境评分
+@property (nonatomic,copy) NSString *book_num;                  //!<预约当前房源的次数
+@property (nonatomic,copy) NSString *building_structure;        //!<房源的建筑结构
+
+@property (nonatomic,copy) NSString *coordinate_x;              //!<经度
+@property (nonatomic,copy) NSString *coordinate_y;              //!<纬度
 
 @property (nonatomic,copy) NSString *is_syserver;               //!<是否已同步到服务端:1-已同步
 
